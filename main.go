417f8904637e3c2fbe0393b36414fa1d7633989e
Ryan <ry.neal@gmail.com>
2021-05-03 14:19:54 -0700

    use config file

--- main.go
+++ main.go
@@ -1,79 +1,85 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 	"strings"
 )
 
-/*
- Usage: go run main.go [-p <port>] path[:status[:content]]
-*/
+type configuration struct {
+	Routes map[string]struct {
+		Headers map[string]string
+		Status  int
+		Payload string
+		Ref     string
+	}
+}
+
 func main() {
 	var port int
 	flag.IntVar(&port, "port", 9090, "the port to run on")
 	flag.Parse()
 
-	args := flag.Args()
-	if len(args) == 0 {
-		fmt.Println("usage: [-port <port>] path[:status[:content]]")
+	if len(flag.Args()) == 0 {
+		fmt.Println("usage: [-port <port>] config.json")
 		os.Exit(1)
 	}
 
+	bs, err := ioutil.ReadFile(flag.Arg(0))
+	if err != nil {
+		log.Fatalf("failed to open the config file %s", flag.Arg(0))
+	}
+
+	var config configuration
+	if err := json.Unmarshal(bs, &config); err != nil {
+		log.Fatalf("failed to unmarshal the config: %+v\n", err)
+	}
+	if len(config.Routes) == 0 {
+		log.Fatalln("no routes defined")
+	}
+
 	mux := http.NewServeMux()
-	for _, a := range args {
-		path, status, content := parsePathDef(a)
+	for path, a := range config.Routes {
+		var body []byte
+		if a.Ref != "" {
+			data, err := ioutil.ReadFile(a.Ref)
+			if err != nil {
+				log.Fatalf("error reading referenced file: %+v\n", err)
+			}
+			body = data
+		}
+		if a.Payload != "" {
+			body = []byte(a.Payload)
+		}
+		status := a.Status
+		if status == 0 {
+			status = http.StatusOK
+		}
 		if !strings.HasPrefix(path, "/") {
 			path = "/" + path
 		}
+		log.Printf("Registering path: %s", path)
+		mux.Handle(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Printf("%s: responding with status %d\n", path, a.Status)
+			for k, v := range a.Headers {
+				w.Header().Set(k, v)
+			}
 
-		mux.Handle(path, buildHandler(path, status, content))
+			w.WriteHeader(status)
+			if _, err := w.Write(body); err != nil {
+				fmt.Printf("error writing response: %+v\n", err)
+			}
+		}))
+
 	}
 	log.Println("Starting to listen on port", port)
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux); err != nil {
 		fmt.Println("Failed to start server:", err)
 		os.Exit(1)
 	}
-}
-
-func buildHandler(path string, status int, content []byte) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s: responding with status %d\n", path, status)
-		w.WriteHeader(status)
-		if _, err := w.Write([]byte(content)); err != nil {
-			fmt.Printf("error writing response: %+v\n", err)
-		}
-	}
-}
-
-func parsePathDef(raw string) (string, int, []byte) {
-	parts := strings.SplitN(raw, ":", 3)
-	if len(parts) == 1 {
-		return parts[0], http.StatusOK, []byte{}
-	}
-
-	code, err := strconv.Atoi(parts[1])
-	if err != nil {
-		panic("failed to split '" + parts[1] + "' into a number: " + err.Error())
-	}
-
-	if len(parts) == 2 {
-		return parts[0], code, []byte{}
-	}
-
-	if strings.HasPrefix(parts[2], "@") {
-		body, err := ioutil.ReadFile(parts[2][1:])
-		if err != nil {
-			panic("failed to open '" + parts[2][1:] + "': " + err.Error())
-		}
-
-		return parts[0], code, body
-	}
-
-	return parts[0], code, []byte(parts[2])
 }
